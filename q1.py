1e9d5cc4ec525001a741e13c4665b0a9f05fbd2e
z5567169 <z5567169@nw-syd-vxdb2>
2024-04-10 14:41:13 +1000

    Question 1

--- q1.py
+++ q1.py
@@ -7,8 +7,8 @@ COMP3311
 Assignment 2
 Pokemon Database
 
-Written by: <YOUR NAME HERE> <YOUR STUDENT ID HERE>
-Written on: <DATE HERE>
+Written by: Petar Iliev z5567169
+Written on: 10/04/2024
 
 File Name: Q1.py
 
@@ -24,13 +24,33 @@ import helpers
 ### Constants
 USAGE = f"Usage: {sys.argv[0]}"
 
+### Queries
+pokemon_query = '''
+    SELECT 
+        g.region,
+        g.name, 
+        COUNT(DISTINCT p.national_id), 
+        COUNT(DISTINCT l.name)
+    FROM Games g
+    JOIN Pokedex p ON p.game = g.id
+    JOIN Locations l ON l.appears_in = g.id
+    GROUP BY g.region, g.name;
+'''
+
 def main(db):
     if len(sys.argv) != 1:
         print(USAGE)
         return 1
-
+    
     # TODO: your code here
+    cur = db.cursor()
+    cur.execute(pokemon_query)
+    result = cur.fetchall()
 
+    print('Region Game              #Pokemon #Locations')
+    for tuple in result:
+        region, game, pokemon_num, location_num = tuple[0], tuple[1], tuple[2], tuple[3]
+        print(f"{region:<6} {game:<17} {pokemon_num:<8} {location_num:<10}")
 
 if __name__ == '__main__':
     exit_code = 0
